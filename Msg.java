4f72a8d09247e922926e751f5ed8db7815668c41
Naveen Kumar P <naveen.pusuluru123@gmail.com>
2026-04-24 07:17:19 +0530

    added 2lines code

--- Msg.java
+++ Msg.java
@@ -2,4 +2,6 @@ public class Msg {
 	Public static void main(string[] args){
 		int a=10, int b=2;
 		system.out.println("Addition is:'+(a+b));
-	}
+	System.out.println("Hello World!");
+ 	System.out.println("I am learning Java.");
+}
