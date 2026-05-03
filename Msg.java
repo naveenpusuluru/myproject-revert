1873374759dde537c9080115781006c00d81fe0c
naveenpusuluru <48553629+naveenpusuluru@users.noreply.github.com>
2026-05-03 19:26:57 +0530

    Update Msg.java

--- Msg.java
+++ Msg.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Msg {
     public static void main(String[] args) {
         // This line prints the message to the console
         System.out.println("Hello, World!");
